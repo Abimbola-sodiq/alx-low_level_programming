bbd2e12567ed1aa1079182e29419e07500b65cea
Abimbola-sodiq <emmysurdz123@gmail.com>
2022-09-11 08:12:34 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,24 +18,4 @@ int main(void)
 
 			return (0);
 
-}i#include <stdio.h>
-
-
-
-/**
- *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
 }
