5fa52949c69dd4bdc1a468d882663f4de68d32a3
Abimbola-sodiq <emmysurdz123@gmail.com>
2022-09-11 11:33:47 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 
 /**
  
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzl
+ 
  
  * Return: 0 if exited properly, non-zero otherwise
  
